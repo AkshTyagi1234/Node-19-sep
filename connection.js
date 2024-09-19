2d3147f54a247faff43ff011dc4ba102dae77ce9
akshtyagi <aksh22tyagi2004@gmail.com>
2024-09-19 12:29:19 +0530

    19 sep

---
+++ connection.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+async function connection() {
+    try {
+        await mongoose.connect(`mongodb://127.0.0.1:27017/batch-3`);
+        console.log("data bas connected....")
+
+    }
+    catch (err) {
+        console.log(err)
+
+    }
+}
+
+module.exports = connection;
